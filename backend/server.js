faed70cc473d0be0ab31e32cdb232c3d736083d7
ShruB1820 <shrutibonde.1820@gmail.com>
2025-08-16 15:05:20 +0530

    feature/admin-panel: CI CD 2

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,10 @@ app.use('/api/admin', adminRoutes);
 
 
 app.get('/health', (_, res) => res.json({ ok: true }));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 if (require.main === module) {
     connectDB();
