e53fd3381738c54f55928deadfcfd63a39cfdeed
ShruB1820 <shrutibonde.1820@gmail.com>
2025-08-16 05:41:17 +0530

    CI CD test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/tasks" element={<Tasks />} />
